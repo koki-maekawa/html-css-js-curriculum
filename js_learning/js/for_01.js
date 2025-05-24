393e0a0ddcad0b471e8126de935c0e4ccff1f559
koki-maekawa <koki.study33@gmail.com>
2025-05-24 11:38:19 +0900

    繰り返し処理_課題01

---
+++ js_learning/js/for_01.js
@@ -0,0 +1,9 @@
+const multipleOfThree =()=>{
+    for(let i = 0; i <= 100; i++){
+        if (i % 3 === 0) {
+            console.log(`${i}:3の倍数です`)
+        }
+    } 
+  }
+ 
+  console.log(multipleOfThree());
